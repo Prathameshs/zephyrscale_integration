6fca6170e2dc63871a9455d03ded57331d4198d5
Prathamesh Sarang <prathamesh.sarang@afourtech.com>
2023-08-22 17:21:01 +0530

    FIX: Update test names to match zephyr scale test ids based on new tests in Zephyr

--- src/com/prathamesh/poc/zephryscaleupdates/FailingTest.java
+++ src/com/prathamesh/poc/zephryscaleupdates/FailingTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class FailingTest {
 
     @Test
-    public void AUD_T14_test_that_fails() {
+    public void AUD_T18_test_that_fails() {
         String s = "This is just a passing test!";
         Assert.assertTrue(false, "This is just a FAILING test!");
     }
--- src/com/prathamesh/poc/zephryscaleupdates/PassingTest.java
+++ src/com/prathamesh/poc/zephryscaleupdates/PassingTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class PassingTest {
 
     @Test
-    public void AUD_T13_test_that_passes() {
+    public void AUD_T17_test_that_passes() {
         String s = "This is just a passing test!";
         Assert.assertTrue(true, "This is just a passing test!");
     }
